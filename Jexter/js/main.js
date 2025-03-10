739c593218907d80b694f1dbddb60a3cdc7dc8d9
RyujinXz <matheuspluralldelas@gmail.com>
2025-03-10 10:38:17 -0300

    com as 5 pages

--- Jexter/js/main.js
+++ Jexter/js/main.js
@@ -1,3 +1,23 @@
+// // Confirmando o Formulário
+// const name = document.getElementById('name')
+// const email = document.getElementById('email')
+// const form = document.getElementById('form')
+// const errorElement = document.getElementById('error')
+
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if (name.value === '' || name.value == null ) {
+//         messages.push('Nome é requerido')
+//     }
+
+//     if (messages.length > 0) {
+//         e.preventDefault()
+//         errorElement.innerText = messages.join(', ')
+//     }
+// })
+
+
+
 /**
  * Init isotope layout and filters
  */
